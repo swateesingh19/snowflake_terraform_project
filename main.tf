a1bcf69593eebd2f27273c49bc4203d9459d9263
swateesingh19 <swatees@gmail.com>
2024-05-10 21:03:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "snowflake_database" "demo_db" {
   comment = "Database for Snowflake Terraform demo"
 }
 resource "snowflake_schema" "demo_schema" {
-  database = "snowflake_database.demo_db.name"
-  name    = "DEMO_SCHEMA"
-  comment = "Schema for Snowflake Terraform demo"
+  database = snowflake_database.demo_db.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
 }
